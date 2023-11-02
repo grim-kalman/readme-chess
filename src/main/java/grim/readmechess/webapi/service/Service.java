290eb147245447241fe2ae447e33b5b1c70b1d87
Grim <grim.e.kalman@gmail.com>
2023-11-02 10:32:09 +0100

    Add Service, COntroller layers and ChessBoard class

---
+++ src/main/java/grim/readmechess/webapi/service/Service.java
@@ -0,0 +1,24 @@
+package grim.readmechess.webapi.service;
+
+@org.springframework.stereotype.Service
+public class Service {
+
+    public void makeMove() {
+
+        //1. MakeMove
+
+        //2. Get best move from API or Stockfish and do it asynchronously?
+
+        //3. Update board state
+
+        //4. Get all possible moves from the new position
+
+        //5. Update the readme with all the new links and pieces
+
+        //6. Make a API call to GitHub to commit a new readme
+
+
+    }
+
+
+}
---
+++ src/test/java/grim/readmechess/ReadmeChessApplicationTests.java
@@ -0,0 +1,14 @@
+package grim.readmechess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadmeChessApplicationTests {
+
+	@Test
+	void shouldPrintTheBoardState() {
+
+	}
+
+}
