0139706d4287dc532380bf3daa8778194391b5f6
Grim <grim.e.kalman@gmail.com>
2023-11-04 16:02:19 +0100

    Add print method and test

--- src/main/java/grim/readmechess/webapi/chessboard/ChessBoard.java
+++ src/main/java/grim/readmechess/webapi/chessboard/ChessBoard.java
@@ -1,4 +1,34 @@
 package grim.readmechess.webapi.chessboard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class ChessBoard {
+    private String fenString;
+    private char[] position;
+
+    public ChessBoard(String fenString) {
+        this.fenString = fenString;
+    }
+
+    public void printChessBoardToConsole() {
+        System.out.println(fenString.substring(0, fenString.indexOf(" "))
+                .replace("1", ".")
+                .replace("2", "..")
+                .replace("3", "...")
+                .replace("4", "....")
+                .replace("5", ".....")
+                .replace("6", "......")
+                .replace("7", ".......")
+                .replace("8", "........")
+                .replace("", " ")
+                .replace(" / ", "\n")
+        );
+    }
+
+    public void setPosition() {
+        String fenString = this.fenString;
+
+    }
 }
