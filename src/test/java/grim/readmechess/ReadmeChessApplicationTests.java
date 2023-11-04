14c5456574825856d3995c9f1b4785915965ef7a
Grim <141512659+grim-kalman@users.noreply.github.com>
2023-11-04 16:02:19 +0100

    Add print method and test

--- src/test/java/grim/readmechess/ReadmeChessApplicationTests.java
+++ src/test/java/grim/readmechess/ReadmeChessApplicationTests.java
@@ -1,14 +1,40 @@
 package grim.readmechess;
 
+import grim.readmechess.webapi.chessboard.ChessBoard;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class ReadmeChessApplicationTests {
 
 	@Test
-	void shouldPrintTheBoardState() {
+	void shouldPrintTheChessBoard() {
+		// Arrange
+		ChessBoard chessBoard = new ChessBoard("rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2");
+		String expectedOutput =
+						"r n b q k b n r\n" +
+						"p p . p p p p p\n" +
+						". . . . . . . .\n" +
+						". . p . . . . .\n" +
+						". . . . P . . .\n" +
+						". . . . . N . .\n" +
+						"P P P P . P P P\n" +
+						"R N B Q K B . R\n";
 
+		ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outputStreamCaptor));
+		PrintStream originalOut = System.out;
+		// Act
+		chessBoard.printChessBoardToConsole();
+		// Assert
+		assertEquals(expectedOutput.trim(), outputStreamCaptor.toString().trim());
+		System.setOut(originalOut);
 	}
-
 }
