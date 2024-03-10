5727ba4af88a17215039ff8eae1968482ae752f2
Grim Kalman <141512659+grim-kalman@users.noreply.github.com>
2024-03-10 19:16:24 +0100

    refactor

--- src/main/java/grim/readmechess/webapi/chessboard/Board.java
+++ src/main/java/grim/readmechess/webapi/chessboard/Board.java
@@ -155,6 +155,10 @@ public class Board {
         }
     }
 
+    private void updateCastlingRights(String move) {
+        
+    }
+
     private void resetEnPassantTarget() {
         enPassantTarget = "-";
     }
