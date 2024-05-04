f94e923d0f16c61388a8d5a115155720e8485a96
Grim Kalman <grim.e.kalman@gmail.com>
2024-05-04 10:16:51 +0200

    chore: adding todo:S

--- src/main/java/grim/readmechess/ReadmeChessApplication.java
+++ src/main/java/grim/readmechess/ReadmeChessApplication.java
@@ -2,16 +2,15 @@ package grim.readmechess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ReadmeChessApplication {
 
 	// TODO: If game is over start a new one.
+	// TODO: Add exception handling for user inputs and logging for internal errors.
 	// TODO: Add counter for number of games played. Won and lost.
 	// TODO: Add game history and replay feature for current game.
-	// TODO: Add exception handling for user inputs and logging for internal errors.
+	// TODO: Refactor code!!!
 	public static void main(String[] args) {
 		SpringApplication.run(ReadmeChessApplication.class, args);
 	}
