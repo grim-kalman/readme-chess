b987f2a21bdfc3d12c468a4b471a214752873f08
Grim Kalman <grim.e.kalman@gmail.com>
2024-05-05 20:07:58 +0200

    chore: add todos

--- src/main/java/grim/readmechess/ReadmeChessApplication.java
+++ src/main/java/grim/readmechess/ReadmeChessApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReadmeChessApplication {
 
 	// TODO: If game is over start a new one.
+	// TODO: Implement promotion for pawns.
 	// TODO: Add exception handling for user inputs and logging for internal errors.
 	// TODO: Add counter for number of games played. Won and lost.
 	// TODO: Add game history and replay feature for current game.
