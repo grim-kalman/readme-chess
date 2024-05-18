18b026979c9d536371fcdb5c45c39d3b9353e90e
Grim Kalman <141512659+grim-kalman@users.noreply.github.com>
2024-05-18 15:16:52 +0200

    adds lombok to other classes

--- src/main/java/grim/readmechess/utils/common/Constants.java
+++ src/main/java/grim/readmechess/utils/common/Constants.java
@@ -1,6 +1,11 @@
 package grim.readmechess.utils.common;
 
 public class Constants {
+
     public static final String WHITE = "w";
     public static final String BLACK = "b";
+
+    private Constants() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
 }
