75cb7ebaae2c276d489b675f918b4dd8685eda90
Grim Kalman <grim.e.kalman@gmail.com>
2024-05-18 14:57:23 +0200

    feat: adds lombok to project to avoid boilerplate code

--- src/main/java/grim/readmechess/controller/Controller.java
+++ src/main/java/grim/readmechess/controller/Controller.java
@@ -3,6 +3,9 @@ package grim.readmechess.controller;
 import grim.readmechess.service.controllerservice.ControllerService;
 import grim.readmechess.service.githubservice.GithubService;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,28 +14,31 @@ import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/api/chess")
+@RequiredArgsConstructor
 public class Controller {
 
     private final ControllerService controllerService;
     private final GithubService githubService;
 
-    public Controller(ControllerService controllerService, GithubService githubService) throws Exception {
-        this.controllerService = controllerService;
-        this.githubService = githubService;
-        this.githubService.updateReadme();
+    @Value("${github.url}")
+    private String githubUrl;
+
+    @PostConstruct
+    public void initialize() throws Exception {
+        githubService.updateReadme();
     }
 
     @GetMapping("/play")
     public RedirectView play(@RequestParam String move) throws Exception {
         controllerService.play(move);
         githubService.updateReadme();
-        return new RedirectView("https://github.com/grim-kalman");
+        return new RedirectView(githubUrl);
     }
 
     @GetMapping("/select")
     public RedirectView select(@RequestParam String square) throws Exception {
         controllerService.select(square);
         githubService.updateReadme();
-        return new RedirectView("https://github.com/grim-kalman");
+        return new RedirectView(githubUrl);
     }
 }
