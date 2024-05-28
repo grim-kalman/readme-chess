c434f6f144cb6612591587d3cf8d3778cb253a2e
Grim Kalman <141512659+grim-kalman@users.noreply.github.com>
2024-05-28 22:23:22 +0200

    feat: adds environmental variables for Azure

--- src/main/java/grim/readmechess/ReadmeChessApplication.java
+++ src/main/java/grim/readmechess/ReadmeChessApplication.java
@@ -6,10 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReadmeChessApplication {
 
-	// TODO: Add exception handling for user inputs and logging for internal errors.
-	// TODO: Add a redirect to the github page if someone hits the wrong endpoint or faulty input.
-	// TODO: Add counter for number of games played. Won and lost.
-	// TODO: Add security to the game.
 	// TODO: Add banter to the game.
 	public static void main(String[] args) {
 		SpringApplication.run(ReadmeChessApplication.class, args);
