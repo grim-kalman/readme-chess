93cf44e20ec9fa1ba7f2c799d451d00db242b1ae
Grim Kalman <141512659+grim-kalman@users.noreply.github.com>
2024-05-30 21:58:01 +0200

    fix: testing changing the env variables

--- src/main/java/grim/readmechess/config/AppConfig.java
+++ src/main/java/grim/readmechess/config/AppConfig.java
@@ -10,25 +10,25 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 @Getter
 public class AppConfig {
-    @Value("${app.chess.engine.path}")
+    @Value("${chess.engine.path}")
     private String enginePath;
 
-    @Value("${app.github.api.url}")
+    @Value("${github.api.url}")
     private String githubApiUrl;
 
-    @Value("${app.github.url}")
+    @Value("${github.url}")
     private String githubUrl;
 
-    @Value("${app.github.readme.path}")
+    @Value("${github.readme.path}")
     private String readmePath;
 
-    @Value("${app.github.branch}")
+    @Value("${github.branch}")
     private String branch;
 
-    @Value("${app.github.owner.repo}")
+    @Value("${github.owner.repo}")
     private String ownerRepo;
 
-    @Value("${app.github.token}")
+    @Value("${github.token}")
     private String token;
 
     @Bean
