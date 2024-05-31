3f92fc113dc1be3f9ff0a80bf96c5b992d0b44fb
Grim Kalman <141512659+grim-kalman@users.noreply.github.com>
2024-05-31 18:44:42 +0200

    feat: adds dependency

--- src/main/java/grim/readmechess/controller/Controller.java
+++ src/main/java/grim/readmechess/controller/Controller.java
@@ -13,23 +13,23 @@ import org.springframework.web.servlet.view.RedirectView;
 public class Controller {
 
     private final AppConfig appConfig;
-    //private final ChessService chessService;
+    private final ChessService chessService;
 
     @GetMapping("/play")
     public RedirectView play(@RequestParam String move) throws Exception {
-        //chessService.play(move);
+        chessService.play(move);
         return new RedirectView(appConfig.getGithubUrl());
     }
 
     @GetMapping("/select")
     public RedirectView select(@RequestParam String square) throws Exception {
-        //chessService.select(square);
+        chessService.select(square);
         return new RedirectView(appConfig.getGithubUrl());
     }
 
     @GetMapping("/new")
     public RedirectView newGame() throws Exception {
-        //chessService.newGame();
+        chessService.newGame();
         return new RedirectView(appConfig.getGithubUrl());
     }
 }
