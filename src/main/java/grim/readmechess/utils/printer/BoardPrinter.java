92ee10794787aa383b731179de96cd692e7e2355
Grim Kalman <141512659+grim-kalman@users.noreply.github.com>
2024-06-02 11:03:36 +0200

    chore: removes the logs

--- src/main/java/grim/readmechess/utils/printer/BoardPrinter.java
+++ src/main/java/grim/readmechess/utils/printer/BoardPrinter.java
@@ -27,12 +27,7 @@ public abstract class BoardPrinter {
     private void populateBoardRepresentation(String position, Piece piece, String[][] boardRepresentation) {
         int col = columnToIndex(position.charAt(0));
         int row = rowToIndex(position.charAt(1));
-        if (piece == null) {
-            log.error("Piece at position {} is null", position);
-        } else {
-            log.info("Piece at position {}: {}", position, piece.getSymbol());
-            boardRepresentation[row][col] = piece.getSymbol();
-        }
+        boardRepresentation[row][col] = piece.getSymbol();
     }
 
     protected abstract String print();
