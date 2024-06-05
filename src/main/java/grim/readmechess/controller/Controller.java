e2b7541e692cdbf53ef0b96fd558ecd9efd55af0
Grim Kalman <grim.e.kalman@gmail.com>
2024-06-05 20:21:16 +0200

    feat: removed loggers

--- src/main/java/grim/readmechess/controller/Controller.java
+++ src/main/java/grim/readmechess/controller/Controller.java
@@ -6,13 +6,11 @@ import grim.readmechess.service.chessservice.ChessService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class Controller {
 
     private final AppConfig appConfig;
@@ -20,21 +18,18 @@ public class Controller {
 
     @GetMapping("/play")
     public RedirectView play(@RequestParam String move, HttpServletRequest request) throws Exception {
-        log.info("Request to play from IP: {}", request.getRemoteAddr());
         chessService.play(move);
         return new RedirectView(appConfig.getGithubUrl());
     }
 
     @GetMapping("/select")
     public RedirectView select(@RequestParam String square, HttpServletRequest request) throws Exception {
-        log.info("Request to select from IP: {}", request.getRemoteAddr());
         chessService.select(square);
         return new RedirectView(appConfig.getGithubUrl());
     }
 
     @GetMapping("/new")
     public RedirectView newGame(HttpServletRequest request) throws Exception {
-        log.info("Request to start new game from IP: {}", request.getRemoteAddr());
         chessService.newGame();
         return new RedirectView(appConfig.getGithubUrl());
     }
