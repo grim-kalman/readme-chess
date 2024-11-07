f2b39770d73ebd1809361a6e3675cb0a0633f7e7
Grim <grim.e.kalman@gmail.com>
2024-11-07 12:10:32 +0100

    makes the instructions a bit more clear and concise

--- src/main/java/grim/readmechess/ReadmeChessApplication.java
+++ src/main/java/grim/readmechess/ReadmeChessApplication.java
@@ -2,8 +2,10 @@ package grim.readmechess;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ReadmeChessApplication {
 
 	public static void main(String[] args) {
